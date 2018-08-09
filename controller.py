488f27458b273ae5a5a8b7f554b07a4fe355dd33
Neill <neillbogie@googlemail.com>
2018-08-09 17:45:53 +0100

    controller sample sends start on shake

--- controller.py
+++ controller.py
@@ -6,6 +6,10 @@ radio.on()
 
 count = 0
 while True:
+    if accelerometer.was_gesture("shake"):
+        radio.send("start")
+        display.show("s")
+        count += 1
     if button_a.was_pressed():
         radio.send("a")
         display.show("a")
