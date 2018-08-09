b293b0b388bd2e58b596ffc66d6fd47dec726a5c
Neill <neillbogie@googlemail.com>
2018-08-09 17:45:39 +0100

    removed colour utils: memory.  no hue cycling for now.

--- Race2pLEDs.py
+++ Race2pLEDs.py
@@ -1,65 +1,29 @@
 from microbit import *
 import neopixel
 import radio
-from random import random
+import random
 
 RED = (64, 0, 0)
 GREEN = (0, 64, 0)
 BLUE = (0, 0, 64)
-yellow = (64, 24, 0)
+YELLOW = (64, 24, 0)
 off = (0, 0, 0)
-
-
-class ColorUtils:
-    @staticmethod
-    def random_colour():
-        rgb = ColorUtils.hsv_to_rgb_256(random(), 1, 1)
-        return rgb[0], rgb[1], rgb[2], 255
-
-    @staticmethod
-    def hsv_to_rgb_256(h, s, v):
-        tmp = ColorUtils.hsv_to_rgb(h, s, v)
-        return int(tmp[0] * 255), int(tmp[1] * 255), int(tmp[2] * 255)
-
-    @staticmethod
-    def hsv_to_rgb(h, s, v):
-        if s == 0.0:
-            return v, v, v
-        i = int(h * 6.0)  # XXX assume int() truncates!
-        f = (h * 6.0) - i
-        p = v * (1.0 - s)
-        q = v * (1.0 - s * f)
-        t = v * (1.0 - s * (1.0 - f))
-        i = i % 6
-        if i == 0:
-            return v, t, p
-        if i == 1:
-            return q, v, p
-        if i == 2:
-            return p, v, t
-        if i == 3:
-            return p, q, v
-        if i == 4:
-            return t, p, v
-        if i == 5:
-            return v, p, q
+colours = [RED, GREEN, BLUE, YELLOW]
 
 
 class Player:
 
-    def __init__(self, hue360, max_pos):
+    def __init__(self, colour, max_pos):
         self.pos = 0
-        self.hue360 = hue360
-        self.colour = ColorUtils.hsv_to_rgb_256(self.hue360 / 360.0, 100, 100)
+        self.colour = colour
         self.max_pos = max_pos
         self.finished = False
 
     def draw(self, pixels):
         pixels[self.pos] = self.colour
 
-    def inc_colour(self, inc):
-        self.hue360 = (self.hue360 + inc) % 360
-        self.colour = ColorUtils.hsv_to_rgb_256(self.hue360 / 360.0, 100, 100)
+    def inc_colour(self, increment):
+        self.colour = random.choice(colours)
 
     def move_forward(self):
         if not self.finished:
@@ -79,7 +43,6 @@ class Game:
 
     def __init__(self):
         self.button_pins = [pin12, pin15, pin14, pin16]
-        self.led_colours = [RED, BLUE, GREEN, yellow]
         self.num_pixels = 6
         self.strips = self.create_pixel_strips(self.num_pixels)
         self.players = [Player(RED, self.num_pixels - 1), Player(BLUE, self.num_pixels - 1)]
@@ -99,16 +62,13 @@ class Game:
     def handle_radio_inputs(self):
         incoming = radio.receive()
         if incoming == 'start':
-            print("radio says start")
             if self.state == Game.STATE_STARTING:
                 self.start_race()
 
         if incoming == 'a':
-            print("radio says move a")
             self.advance_player(0)
 
         if incoming == 'b':
-            print("radio says move b")
             self.advance_player(1)
 
     def advance_player(self, ix):
