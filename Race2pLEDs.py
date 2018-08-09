71d20e0a6cc61637279530a906993beb84e762b1
Neill <neillbogie@googlemail.com>
2018-08-09 21:18:59 +0100

    add race-start traffic lights. switch to 60 pxl /strip

--- Race2pLEDs.py
+++ Race2pLEDs.py
@@ -7,8 +7,9 @@ RED = (64, 0, 0)
 GREEN = (0, 64, 0)
 BLUE = (0, 0, 64)
 YELLOW = (64, 24, 0)
+ORANGE = (64, 64, 0)
 off = (0, 0, 0)
-colours = [RED, GREEN, BLUE, YELLOW]
+colours = [RED, ORANGE, GREEN, BLUE, YELLOW]
 
 
 class Player:
@@ -22,7 +23,7 @@ class Player:
     def draw(self, pixels):
         pixels[self.pos] = self.colour
 
-    def inc_colour(self, increment):
+    def cycle_colour(self):
         self.colour = random.choice(colours)
 
     def move_forward(self):
@@ -39,24 +40,42 @@ class Player:
 class Game:
     STATE_FINISHED = 'finished'
     STATE_STARTING = 'starting'
+    STATE_LIGHTS = 'lights'
     STATE_RACING = 'racing'
 
     def __init__(self):
         self.button_pins = [pin12, pin15, pin14, pin16]
-        self.num_pixels = 6
+        self.num_pixels = 60
         self.strips = self.create_pixel_strips(self.num_pixels)
         self.players = [Player(RED, self.num_pixels - 1), Player(BLUE, self.num_pixels - 1)]
         self.game_over = False
         self.dirty = [False, False]
         self.state = Game.STATE_STARTING
+        self.winner_ix = 10
 
     def create_pixel_strips(self, num):
-        return [neopixel.NeoPixel(pin13, num), neopixel.NeoPixel(pin11, num)]
+        #pin 13 for bit commander
+        return [neopixel.NeoPixel(pin0, num), neopixel.NeoPixel(pin1, num)]
 
-    def check_for_win(self):
-        return False
+    def strip_show_colour(self, strip, colour):
+        for i in range(self.num_pixels):
+            strip[i] = colour
+            strip.show()
+
+    def clear_strips(self):
+        for s in self.strips:
+            s.clear()
+            s.show()
 
     def start_race(self):
+        self.state = Game.STATE_LIGHTS
+        for (c, msg) in [(RED, "red"), (ORANGE, "amber"), (GREEN, "green")]:
+            for s in self.strips:
+                radio.send(msg)
+                self.strip_show_colour(s, c)
+                sleep(500)
+        self.clear_strips()
+
         self.state = Game.STATE_RACING
 
     def handle_radio_inputs(self):
@@ -71,15 +90,21 @@ class Game:
         if incoming == 'b':
             self.advance_player(1)
 
+    def win_race(self, ix):
+        self.winner_ix = ix
+        radio.send("p1won" if ix == 0 else "p2won")
+        # TODO: animate win by player colour
+        self.state = Game.STATE_FINISHED
+
     def advance_player(self, ix):
         if self.state == Game.STATE_RACING:
             self.players[ix].move_forward()
             if self.players[ix].is_finished():
-                self.state = Game.STATE_FINISHED
+                self.win_race(ix)
             self.dirty[ix] = True
 
     def spin_player_colour(self, ix):
-        self.players[ix].inc_colour(5)
+        self.players[ix].cycle_colour()
         self.dirty[ix] = True
 
     def handle_inputs(self):
@@ -98,13 +123,12 @@ class Game:
 
     def update(self):
         self.handle_inputs()
-        self.check_for_win()
 
     def draw_game_over(self):
         for s in self.strips:
-            s[1] = (200, 0, 200)
-            s[2] = (0, 200, 200)
-            s[3] = (0, 0, 200)
+            s[1] = RED
+            s[2] = GREEN
+            s[3] = BLUE
             s.show()
 
     def redraw_dirty(self):
@@ -113,16 +137,15 @@ class Game:
         else:
             for i in range(2):
                 if self.dirty[i]:
+                    self.strips[i].clear()
                     # TODO: check strip is passed by reference
                     self.players[i].draw(self.strips[i])
                     self.dirty[i] = False
 
-        for s in self.strips:
-            s.show()
+                    self.strips[i].show()
 
     def game_loop(self):
         while True:
-            
             self.update()
             self.redraw_dirty()
 
@@ -130,5 +153,6 @@ class Game:
 radio.on()
 radio.config(group=22)
 radio.send('i_am_awake')
+display.scroll("pyrace ch22", 50)
 game = Game()
 game.game_loop()
