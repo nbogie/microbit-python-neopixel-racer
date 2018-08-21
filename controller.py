3fbaea4268134a75e51daded3341a9bb8625d61b
Neill <neillbogie@googlemail.com>
2018-08-22 00:38:51 +0100

    rem unnecessary counting from sample controller

--- controller.py
+++ controller.py
@@ -4,18 +4,14 @@ import radio
 radio.config(group=21)
 radio.on()
 display.scroll('tx pyrace', 50)
-count = 0
 
 while True:
     if accelerometer.was_gesture("shake"):
         radio.send("start")
         display.show("s")
-        count += 1
     if button_a.was_pressed():
         radio.send("a")
         display.show("a")
-        count += 1
     if button_b.was_pressed():
         radio.send("b")
         display.show("b")
-        count += 1
