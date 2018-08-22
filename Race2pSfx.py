de484530b0ada140819095f24cec0736495c945d
Neill <neillbogie@googlemail.com>
2018-08-22 02:31:24 +0100

    finished race sfx example

--- Race2pSfx.py
+++ Race2pSfx.py
@@ -13,17 +13,20 @@ def handle_radio_inputs():
     if incoming == 'soon':
         display.show('s')
     if incoming == 'red':
+        music.pitch(880, 30)
         display.show('r')
     if incoming == 'amber':
+        music.pitch(880, 30)
         display.show('a')
     if incoming == 'green':
+        music.pitch(1760, 120)
         display.show('g')
     if incoming == 'go':
         display.show('!')
-        music.play(music.BA_DING)
+        music.play(music.BA_DING)        
     if incoming == 'p1moved':
         display.show('<')
-        music.pitch(660, 30)
+        music.pitch(660, 30)        
     if incoming == 'p2moved':
         display.show('>')
         music.pitch(440, 30)
@@ -32,12 +35,13 @@ def handle_radio_inputs():
         music.play(music.POWER_UP)
     if incoming == 'p2won':
         display.show('2')
-        music.play(music.PYTHON)
+        music.play(music.JUMP_DOWN)
 
 radio.on()
 radio.config(group=21)
 display.scroll("sfx:21", 50)
+music.set_tempo(ticks=4, bpm=240)
 
 while True:
     handle_radio_inputs()
-    sleep(50)
+    sleep(10)
