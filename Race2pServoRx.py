681bfd911507343abad22c4238fb2c0107ada126
Neill <neillbogie@googlemail.com>
2018-09-10 18:04:15 +0100

    racer servo finished.  servo likes bigger wiggle travels.

--- Race2pServoRx.py
+++ Race2pServoRx.py
@@ -1,13 +1,7 @@
 from microbit import *
 import radio
 
-# Don't ask it to go to 0 - may stop whole thing
-
-def player1WinWiggle():
-    wiggleBetween(15, 5, 4)
-
-def player2WinWiggle():
-    wiggleBetween(165, 175, 4)
+# Note: Don't ask servo to go to 0 or 180.
 
 def processMsg(msg):
     if (msg == "init"):
@@ -21,12 +15,21 @@ def processMsg(msg):
     elif (msg == "tied"):
         pin1.write_analog(90)
     elif (msg == "p1won"):
+        # pin1.write_analog(170)
         player1WinWiggle()
     elif (msg == "p2won"):
+        # pin1.write_analog(10)
         player2WinWiggle()
 
 def toggleMarkerPixel():
-    pass
+    display.set_pixel(0, 0, not display.get_pixel(0, 0))
+
+def player1WinWiggle():
+    wiggleBetween(175, 115, 4)
+
+def player2WinWiggle():
+    wiggleBetween(45, 5, 4)
+
 
 def preGameWiggle():
     pin1.write_analog(175)
@@ -38,25 +41,17 @@ def preGameWiggle():
 def wiggleBetween(angle1, angle2, repetitions):
     for i in range(repetitions):
         pin1.write_analog(angle1)
-        sleep(300)
+        sleep(500)
         pin1.write_analog(angle2)
-        sleep(300)
-
-
-def demo():
-    while True:
-        pin1.write_analog(180)
-        sleep(1000)
-        pin1.write_analog(1)
-        sleep(1000)    
+        sleep(500)
 
 radio.on()
 radio.config(group=21)
-display.show('s')
+display.scroll('servo 21', 50)
 sleep(500)
 
 pin1.set_analog_period(20)
-# demo()
+
 while True:
     incoming = radio.receive()
     if incoming is not None:
